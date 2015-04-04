9f8c4ddea31e778723eda46a5ad384c9ab5247f6
Sebastian Vråle <seb@terminator.net>
2015-04-04 04:46:45 +0200

    Level jump (cheat) buttons

--- Assets/Scripts/LevelCheater.cs
+++ Assets/Scripts/LevelCheater.cs
@@ -3,10 +3,25 @@ using System.Collections;
 
 public class LevelCheater : MonoBehaviour
 {
+    public GameObject LevelButtonPrefab;
+    public Transform ButtonContainer;
 
     void Start()
     {
+        for (int i = 0; i < Application.levelCount; i++) {
+            if (i != Application.loadedLevel) {
+                MakeButton(i);
+            }
+        }
 
+    }
+
+    private void MakeButton(int levelToLoad)
+    {
+        GameObject levelButton = Instantiate(LevelButtonPrefab);
+        levelButton.GetComponent<LevelLoadButtonBehaviour>().SetLevelToLoad(levelToLoad);
+
+        levelButton.transform.SetParent(ButtonContainer);
     }
 
     void Update()
---
+++ Assets/Scripts/LevelLoadButtonBehaviour.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class LevelLoadButtonBehaviour : MonoBehaviour
+{
+
+    private int levelToLoad;
+
+    public void SetLevelToLoad(int i)
+    {
+        levelToLoad = i;
+
+        GetComponentInChildren<Text>().text = "Level " + (i + 1);
+    }
+
+    public void LoadOurLevel()
+    {
+        Application.LoadLevel(levelToLoad);   
+    }
+}
